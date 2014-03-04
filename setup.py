10e10311530e61a0a9efe5b8735f625e0059e9a4
joke2k <joke2k@gmail.com>
2014-03-04 18:06:01 +0100

    inc version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-widgets',
-    version='0.1.1-dev',
+    version='0.1.2-dev',
     packages=['widgets'],
     include_package_data=True,
     license='BSD License',  # example license

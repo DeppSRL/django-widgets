6b7909261ba36429a8cba78ceaeb99d68a02a148
joke2k <joke2k@gmail.com>
2014-03-04 18:01:51 +0100

    add fix for old IE

--- widgets/static/js/widgets.js
+++ widgets/static/js/widgets.js
@@ -1,5 +1,38 @@
 var LOADER_NS = 'widgets_loader_ns';
 
+window.hasOwnProperty = window.hasOwnProperty || Object.prototype.hasOwnProperty;
+
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement, fromIndex) {
+        if ( this === undefined || this === null ) {
+            throw new TypeError( '"this" is null or not defined' );
+        }
+
+        var length = this.length >>> 0; // Hack to convert object.length to a UInt32
+
+        fromIndex = +fromIndex || 0;
+
+        if (Math.abs(fromIndex) === Infinity) {
+            fromIndex = 0;
+        }
+
+        if (fromIndex < 0) {
+            fromIndex += length;
+            if (fromIndex < 0) {
+                fromIndex = 0;
+            }
+        }
+
+        for (;fromIndex < length; fromIndex++) {
+            if (this[fromIndex] === searchElement) {
+                return fromIndex;
+            }
+        }
+
+        return -1;
+    };
+}
+
 if (typeof String.prototype.endsWith !== 'function') {
     String.prototype.endsWith = function(suffix) {
         'use strict';
